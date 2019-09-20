10e000aae5c0a2288e907badc3c6182618056563
Vishal Bhamare <42106299+v1shaL-b@users.noreply.github.com>
2019-09-20 16:24:31 +0530

    add initial cv website files

---
+++ script.js
@@ -0,0 +1,50 @@
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+console.log(1);
+console.log(2);
+console.log(3);
+
+let email = 'vbhamare11@gmail.com';
+let fname = 'Vishal';
+let lname = 'Bhamare';
+let fulName = fname + ' ' + lname;
+console.log('single quote');
+console.log("double quote");
+console.log( email );
+console.log(fulName);
+
+
+console.log("get character");
+console.log(fulName[0]);
+console.log(fulName[1]);
+console.log(fulName[2]);
+console.log(fulName[3]);
+
+console.log(fulName.length);
+
+////////////////////////
+
+
+console.log(fulName.toUpperCase());
+
+let result = fulName.toLowerCase();
+
+console.log(result);
+
+let index = email.indexOf('@');
+
+console.log(index);
+
+
+
+
+
+
+
+
+
+
+
+
+
